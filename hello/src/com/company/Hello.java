51edfef752f2077ceacf68f6ed1754280ac12252
Tom <thomas.spencer@codurance.com>
2021-09-16 13:00:54 +0100

    add starter for hello and primitive-types

---
+++ hello/src/com/company/Hello.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Hello {
+
+    public static void main(String[] args) {
+      System.out.println("Hello, Tom");
+      int myFirstNumber = (10 + 5) + (2 * 10);
+      int mySecondNumber = 12;
+      int myThirdNumber = myFirstNumber * 2;
+
+      int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+      int myDifference = 1000 - myTotal;
+
+      System.out.println(myFirstNumber);
+      System.out.println(myTotal);
+      System.out.println(myDifference);
+
+      if (args.length > 0){
+        System.out.println("args =" + args[0]);
+      }else {
+        System.out.println("No args!");
+      }
+    }
+}
